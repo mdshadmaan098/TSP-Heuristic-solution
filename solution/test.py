a5b5857812f308f9d51f4bad0053ac9d913a4b31
shadmaan <mshadmaan.dev@gmail.com>
2019-10-14 13:42:49 +0530

    creating base class to handle initial tsp mechanism,added data

---
+++ solution/test.py
@@ -0,0 +1,23 @@
+
+from data import matrixs
+from base import TSP
+
+
+names = [
+    "Mumbai",
+    "Delhi",
+    "Bangalore",
+    "Hyderabad",
+    "Ahmedabad",
+    "Surat",
+    "Pune",
+    "Kanpur",
+    "Nagpur",
+    "Bhopal",
+    "Patna",
+    "Chandigarh",
+    "Noida"
+]
+# Load the distances
+TSP.setEdges(matrixs)
+
