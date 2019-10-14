faa5f5c872e87a78a1471f65d64d495d27b49a1f
shadmaan <mshadmaan.dev@gmail.com>
2019-10-14 15:27:00 +0530

    added tour improvement techniques

--- solution/test.py
+++ solution/test.py
@@ -1,7 +1,7 @@
 
 from data import matrixs
 from base import TSP
-
+from optimise import KOpt
 
 names = [
     "Mumbai",
@@ -17,7 +17,13 @@ names = [
     "Patna",
     "Chandigarh",
     "Noida"
+    
 ]
 # Load the distances
 TSP.setEdges(matrixs)
+initial_tour = KOpt(range(len(matrixs)))
+path, cost = initial_tour.optimise()
+print("Best path has cost: {}".format(cost))
+print([names[i] for i in path])
+
 
